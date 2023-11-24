71313f33754a45e87d32a9ea3698e05465af4728
David Nguyen <davidngu28@gmail.com>
2023-11-24 12:08:51 +1100

    feat: wip

--- packages/prisma/migrations/20231123012355_mergeme/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "Document" ADD COLUMN     "teamId" INTEGER;
-
--- AddForeignKey
-ALTER TABLE "Document" ADD CONSTRAINT "Document_teamId_fkey" FOREIGN KEY ("teamId") REFERENCES "Team"("id") ON DELETE SET NULL ON UPDATE CASCADE;
--- packages/prisma/migrations/20231124010816_team_wip/migration.sql
+++ packages/prisma/migrations/20231124010816_team_wip/migration.sql
@@ -1,11 +1,14 @@
 -- CreateEnum
+CREATE TYPE "TeamStatus" AS ENUM ('ACTIVE', 'INACTIVE');
+
+-- CreateEnum
 CREATE TYPE "TeamMemberRole" AS ENUM ('ADMIN', 'MANAGER', 'MEMBER');
 
 -- CreateEnum
-CREATE TYPE "TeamMemberStatus" AS ENUM ('ACTIVE', 'PENDING');
-
--- CreateEnum
 CREATE TYPE "TeamMemberInviteStatus" AS ENUM ('ACCEPTED', 'PENDING');
+
+-- AlterTable
+ALTER TABLE "Document" ADD COLUMN     "teamId" INTEGER;
 
 -- CreateTable
 CREATE TABLE "Team" (
@@ -15,7 +18,7 @@ CREATE TABLE "Team" (
     "avatar" TEXT,
     "emails" TEXT[],
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "ownerId" INTEGER NOT NULL,
+    "ownerUserId" INTEGER NOT NULL,
 
     CONSTRAINT "Team_pkey" PRIMARY KEY ("id")
 );
@@ -24,7 +27,7 @@ CREATE TABLE "Team" (
 CREATE TABLE "TeamMember" (
     "id" SERIAL NOT NULL,
     "teamId" INTEGER NOT NULL,
-    "status" "TeamMemberStatus" NOT NULL DEFAULT 'PENDING',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "role" "TeamMemberRole" NOT NULL,
     "userId" INTEGER NOT NULL,
 
@@ -35,6 +38,7 @@ CREATE TABLE "TeamMember" (
 CREATE TABLE "TeamMemberInvite" (
     "id" SERIAL NOT NULL,
     "teamId" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "email" TEXT NOT NULL,
     "status" "TeamMemberInviteStatus" NOT NULL DEFAULT 'PENDING',
     "role" "TeamMemberRole" NOT NULL,
@@ -56,13 +60,16 @@ CREATE UNIQUE INDEX "TeamMemberInvite_token_key" ON "TeamMemberInvite"("token");
 CREATE UNIQUE INDEX "TeamMemberInvite_teamId_email_key" ON "TeamMemberInvite"("teamId", "email");
 
 -- AddForeignKey
-ALTER TABLE "Team" ADD CONSTRAINT "Team_ownerId_fkey" FOREIGN KEY ("ownerId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "Document" ADD CONSTRAINT "Document_teamId_fkey" FOREIGN KEY ("teamId") REFERENCES "Team"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Team" ADD CONSTRAINT "Team_ownerUserId_fkey" FOREIGN KEY ("ownerUserId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "TeamMember" ADD CONSTRAINT "TeamMember_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "TeamMember" ADD CONSTRAINT "TeamMember_teamId_fkey" FOREIGN KEY ("teamId") REFERENCES "Team"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "TeamMember" ADD CONSTRAINT "TeamMember_teamId_fkey" FOREIGN KEY ("teamId") REFERENCES "Team"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "TeamMemberInvite" ADD CONSTRAINT "TeamMemberInvite_teamId_fkey" FOREIGN KEY ("teamId") REFERENCES "Team"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "TeamMemberInvite" ADD CONSTRAINT "TeamMemberInvite_teamId_fkey" FOREIGN KEY ("teamId") REFERENCES "Team"("id") ON DELETE CASCADE ON UPDATE CASCADE;
